c9acbac0437f71ac70f19a11b98949510b56c3e2
Shreya15-02 <76565705+Shreya15-02@users.noreply.github.com>
2021-06-04 13:33:36 +0530

    Add files via upload

---
+++ CSS_ASSIGNMENT_1.vb
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        section
+        {
+            text-align:center;
+            font-family: Algerian;
+            color: #99722b;
+            padding:8px;
+            Margin:8px;
+            
+        }
+        h1
+        {
+            text-decoration: underline;
+            text-align: center;
+            font-family: Algerian;
+            color: #99722b;
+            font-weight: bold;
+            font-size:32px;
+            margin:30px;
+        }
+        p
+        {
+            text-align:center;
+            font-family: Algerian;
+            color: #99722b;
+            padding:4px;
+        }
+        #main_div
+        {
+            background-image: url(image/backgrounds.jpg);
+            background-position:center;
+            background-attachment:scroll;
+            background-repeat: no-repeat;
+            height:680px;
+            width:1000px;
+        }
+    </style>
+</head>
+<body>
+    <div id="main_div">
+          &nbsp;
+        <h1>Super Mom</h1>
+        <section>       
+            Mom, you're a wonderful mother,
+            <br>
+            So gentle, yet so strong.
+            <br>
+            The many ways you show you care
+            <br>
+            Always make me feel I belong.
+        </section>
+        <section>
+            You're patient when I'm foolish;
+            <br>
+            You give guidance when I ask;
+            <br>
+            It seems you can do most anything;
+            <br>
+            You're the master of every task.
+        </section>
+        <section>
+            You're a dependable source of comfort;
+            <br>
+            You're my cushion when I fall.
+            <br>
+            You help in times of trouble;
+            <br>
+            You support me whenever I call.
+        </section>
+        <section>
+            I love you more than you know;
+            <br>
+            You have my total respect.
+            <br>
+            If I had my choice of mothers,
+            <br>
+            You'd be the one I'd select!
+        </section>
+        <p>--By Joanna Fuchs</p>
+    </div>
+</body>
+</html>
