b688442f21cdada5111da5e23fd69424ae253169
Shreya15-02 <76565705+Shreya15-02@users.noreply.github.com>
2021-06-15 02:16:48 +0530

    Add files via upload

---
+++ style7.vb
@@ -0,0 +1,61 @@
+
+ *{
+    box-sizing: border-box;
+  }
+  .mainn_div
+    {
+        background-position:center;
+        background-attachment:scroll;
+        display:flex;
+        width:100%;
+        height:680px;
+        flex-wrap:wrap;
+        align-content:stretch;
+        
+    }
+  .box{
+       width:33.33%;
+       text-align: left;
+      padding-top:20px;
+     padding-left:30px;
+     padding-right:30px;
+     padding-bottom: 20px;
+     height:640px;
+  }
+
+.serial{
+    background-clip: border-box;  
+      background-color: #EEB500;
+      color: white;
+  text-align: center;
+  width: 30px;
+  height: 30px;
+  margin:10px;
+  }
+  .abc{
+      color: #0A5A78;
+      text-align: left;
+      font-weight: bold;
+        width: 100%;
+        margin-top: 10px;
+  
+  }
+  .def{
+    text-align: center;
+    color: white;
+    width: 100%;
+      background-color:red;
+      margin-top: 5px;
+  }
+  
+  .explanation{
+      color: #4C4C4C;
+      font-size: large;
+      margin-top:20px;
+      font-family: sans-serif;
+    }
+    body{
+
+      font-size:20px;
+      padding:0 20px 30px 0;
+  }
